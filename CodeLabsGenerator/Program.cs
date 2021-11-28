3c06769c71a309137ad4212b3f03c7b5ac23920e
TRMO <tromortensen@hotmail.com>
2021-11-28 15:56:41 +0100

    Moved BasePage to fit folder levels.

--- CodeLabsGenerator/Program.cs
+++ CodeLabsGenerator/Program.cs
@@ -6,7 +6,7 @@
         {
             
             string folderPathToMdSteps = @"C:\TRMO\RiderProjects\CodeLabs\MarkDownSources\CsharpSockets";
-            string basePagePath = @"C:\TRMO\RiderProjects\CodeLabs\BasePageSource\BasePage.html";
+            string basePagePath = @"C:\TRMO\RiderProjects\CodeLabs\BasePageSource\extralevelfolder\BasePage.html";
             Generator.GenerateHtmlPageFromMdFiles(folderPathToMdSteps, basePagePath);
         }
     }
