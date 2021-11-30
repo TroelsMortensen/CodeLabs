6d337549b2a301312306da9e46e060ebffa3bcda
Troels Mortensen <tromortensen@hotmail.com>
2021-11-30 10:27:39 +0100

    Dunno, maybe more minor changes.

--- CodeLabsGenerator/Generator.cs
+++ CodeLabsGenerator/Generator.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -37,12 +36,14 @@ namespace CodeLabsGenerator
 
         private static void InsertStepsOverview(string pathToMdSteps, StringBuilder mainBuilder)
         {
+            // TODO at some point, remove any leading numbering of file name, and prefix automatically by the i variable in the loop. I foresee that when changes are made to the tutorial steps, we will see numberings like 3.1, and 3.05 etc. The final output should number better
+            
             string[] fileNames = Directory.GetFiles(pathToMdSteps, "*.md");
             StringBuilder sb = new();
             for (int i = 0; i < fileNames.Length; i++)
             {
                 string fileName = ExtractMdFileName(fileNames, i);
-                sb.Append($"<li class=\"step\" onclick=\"setTab({i})\">{fileName}</li>").Append("\n");
+                sb.Append($"<li class=\"step\" onclick=\"setTab({i})\">{fileName}</li>").Append('\n');
             }
 
             mainBuilder.Replace("###STEPOVERVIEW###", sb.ToString());
